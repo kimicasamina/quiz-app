e476eaa7cf673ef72ed9836e9a6d4d9b1ebd8cfa
kimicasamina <kimicasamina@gmail.com>
2023-02-06 10:18:21 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const questionsAndAnswers = [
+  {
+    question: 'In what year were the first Air Jordan sneakers released?',
+    choiceA: '1984',
+    choiceB: '1992',
+    choiceC: '1800',
+    choiceD: '1999',
+    correctAns: '1984'
+  },
+  {
+    question: 'In which European city would you find Orly airport?',
+    choiceA: 'Berlin',
+    choiceB: 'London',
+    choiceC: 'Paris',
+    choiceD: 'Philippines'
+  }
+]
+
+
+
+
+// questions from obj
+// select answers
+// check if answer is correct
+// at the end show score
