dc24d52a3a31b362e9e73bbb5b4f46713c4c9941
kimicasamina <kimicasamina@gmail.com>
2023-02-06 14:28:21 +0800

    completed quizapp

--- js/script.js
+++ js/script.js
@@ -1,40 +1,119 @@
-const main = document.querySelector('#main')
-
-const questionsAndAnswers = [
+const quizData = [
   {
     question: 'In what year were the first Air Jordan sneakers released?',
-    choiceA: '1984',
-    choiceB: '1992',
-    choiceC: '1800',
-    choiceD: '1999',
-    correctAns: '1984'
+    a: '1984',
+    b: '1992',
+    c: '1800',
+    d: '1999',
+    answer: '1984'
   },
   {
     question: 'In which European city would you find Orly airport?',
-    choiceA: 'Berlin',
-    choiceB: 'London',
-    choiceC: 'Paris',
-    choiceD: 'Philippines'
+    a: 'Berlin',
+    b: 'London',
+    c: 'Paris',
+    d: 'Philippines',
+    answer: 'Paris'
+  },
+  {
+    question: 'Water has a pH level of around',
+    a: '4',
+    b: '6',
+    c: '7',
+    d: '9',
+    answer: '7'
+  },
+  {
+    question: 'What did the Crocodile swallow in Peter Pan',
+    a: 'A pair of scissors',
+    b: 'An alarm clock',
+    c: 'A donut',
+    d: 'Peter Pan',
+    answer: 'An alarm clock'
+  },
+  {
+    question: 'Which horoscope sign has a crab',
+    a: 'sagitarius',
+    b: 'taurus',
+    c: 'scorpion',
+    d: 'cancer',
+    answer: 'cancer'
+  },
+  {
+    question: 'The Statue of Liberty was given to the US by which country?',
+    a: 'Malaysia',
+    b: 'United States',
+    c: 'France',
+    d: 'Sweden',
+    answer: 'France'
+  },
+  {
+    question: 'Who invented scissors',
+    a: 'Leonardo Da Vinci',
+    b: 'Albert Eintein',
+    c: 'Marie Curie',
+    d: 'Isaac Newton',
+    answer: 'Leonardo Da Vinci'
+  },
+  {
+    question: 'In which film did father and son actors Will and Jaden Smith first appear together',
+    a: 'Fight Club',
+    b: 'The pursuit of happiness',
+    c: 'Into the wild',
+    d: 'The karate kid',
+    answer: 'Leonardo Da Vinci'
   }
 ]
 
-questionsAndAnswers.forEach(qAndA => {
-  let html = `
-  <div class="question">
-        ${qAndA.question}
-      </div>
-      <div class="choices">
-        <div class="choice a">${qAndA.choiceA}.</div>
-        <div class="choice b">${qAndA.choiceB}</div>
-        <div class="choice c">${qAndA.choiceC}</div>
-        <div class="choice d">${qAndA.choiceD}</div>
-      </div>
-  `
-  main.innerHTML = html
+const main = document.querySelector('#main')
+const question = document.querySelector('.question')
+const a = document.querySelector('.a')
+const b = document.querySelector('.b')
+const c = document.querySelector('.c')
+const d = document.querySelector('.d')
+const choices = document.querySelector('.choices')
+const score = document.querySelector('#score')
+
+let quizIndex = 0
+let currentQuizData
+let playersAnswer
+let playersScore = 0
+
+
+loadQuiz()
+
+function loadQuiz(){
+  currentQuizData = quizData[quizIndex]
+  console.log(currentQuizData['question'])
+  question.innerText = currentQuizData['question']
+  console.log(a.childNodes[1].textContent)
+  a.childNodes[1].textContent = currentQuizData['a']
+  b.childNodes[1].textContent = currentQuizData['b']
+  c.childNodes[1].textContent = currentQuizData['c']
+  d.childNodes[1].textContent = currentQuizData['d']
+  console.log(quizIndex)
+}
+
+
+choices.addEventListener('click', (e) => {
+  playersAnswer = e.target.childNodes[1].textContent
+  checkAnswer(playersAnswer, currentQuizData['answer'])
+  
+  if (quizIndex < quizData.length - 1){
+
+    loadQuiz()
+  } else {
+    window.alert(`You completed the quiz! Your total score is ${playersScore}`)
+
+  }
+  quizIndex++
 })
 
 
-// questions from obj
-// select answers
-// check if answer is correct
-// at the end show score
+function checkAnswer(playersAnswer, answer){
+  if (playersAnswer == answer){
+    console.log('correct')
+    playersScore++
+    score.innerText = playersScore
+  }
+}
